6cb27fc82fba1036d70e3b01388232f1298e795c
Rui Oliveira <ruimail24@gmail.com>
2022-06-28 00:53:10 +0100

    Add a simple build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+BR_RELEASE="buildroot-2022.02.2"
+
+if [ ! -e ${BR_RELEASE}.tar.gz ]; then
+  wget https://buildroot.org/downloads/${BR_RELEASE}.tar.gz
+  tar xf ${BR_RELEASE}.tar.gz
+fi
+make -C ${BR_RELEASE} BR2_EXTERNAL=../br2_external O=../build X6100_defconfig
+make -C build -j$((`nproc`))
