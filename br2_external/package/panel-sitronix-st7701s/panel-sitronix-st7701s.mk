0c93dcb4a1d7ea1cb1fae8e97fb0e6cb933bd09b
Rui Oliveira <ruimail24@gmail.com>
2022-06-28 15:59:34 +0100

    Fix PANEL_SITRONIX_ST7701S's package definition

--- br2_external/package/panel-sitronix-st7701s/panel-sitronix-st7701s.mk
+++ br2_external/package/panel-sitronix-st7701s/panel-sitronix-st7701s.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-ST7701S_SITE = https://github.com/ruilvo/panel-jinglitai-jlt4013a.git
-ST7701S_VERSION = origin/master
-ST7701S_SITE_METHOD = git
-ST7701S_AUTORECONF = YES
+PANEL_SITRONIX_ST7701S_SITE = https://github.com/ruilvo/panel-jinglitai-jlt4013a.git
+PANEL_SITRONIX_ST7701S_VERSION = origin/master
+PANEL_SITRONIX_ST7701S_SITE_METHOD = git
+PANEL_SITRONIX_ST7701S_AUTORECONF = YES
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
