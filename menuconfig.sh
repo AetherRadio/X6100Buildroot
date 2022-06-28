f0fa618e40e56079e93ec728b932a1f7350f90e6
Rui Oliveira <ruimail24@gmail.com>
2022-06-28 11:44:32 +0100

    Add helper scripts for menuconfig

--- menuconfig.sh
+++ menuconfig.sh
@@ -6,5 +6,4 @@ if [ ! -e ${BR_RELEASE}.tar.gz ]; then
   wget https://buildroot.org/downloads/${BR_RELEASE}.tar.gz
   tar xf ${BR_RELEASE}.tar.gz
 fi
-make -C ${BR_RELEASE} BR2_EXTERNAL=../br2_external O=../build X6100_defconfig
-make -C build -j$((`nproc`))
+make -C ${BR_RELEASE} BR2_EXTERNAL=../br2_external O=../build menuconfig
