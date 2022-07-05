4b9d638a06ab250e312bdb946994a7bc6b740b58
Rui Oliveira <ruimail24@gmail.com>
2022-07-05 16:23:19 +0100

    Fix the panel's package declaration

--- br2_external/package/panel-sitronix-st7701s/panel-sitronix-st7701s.mk
+++ br2_external/package/panel-sitronix-st7701s/panel-sitronix-st7701s.mk
@@ -5,9 +5,8 @@
 ################################################################################
 
 PANEL_SITRONIX_ST7701S_VERSION = master
-PANEL_SITRONIX_ST7701S_SOURCE = panel-jinglitai-jlt4013a-$(PANEL_SITRONIX_ST7701S_VERSION).tar.gz
+PANEL_SITRONIX_ST7701S_SOURCE = $(PANEL_SITRONIX_ST7701S_VERSION).tar.gz
 PANEL_SITRONIX_ST7701S_SITE = https://github.com/ruilvo/panel-jinglitai-jlt4013a/archive
-PANEL_SITRONIX_ST7701S_SITE_METHOD = wget
 PANEL_SITRONIX_ST7701S_AUTORECONF = YES
 PANEL_SITRONIX_ST7701S_LICENSE = GPLv2
 
