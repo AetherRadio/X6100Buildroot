af1f3689f0327f0532861b2b2ed028bb4cc4a683
Rui Oliveira <ruimail24@gmail.com>
2022-07-08 15:05:45 +0100

    Move boot.cmd

---
+++ br2_external/board/X6100/u-boot/boot.cmd
@@ -0,0 +1,7 @@
+echo "------------ X6100 boot script ------------"
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 fbcon=rotate:3 \
+video=VGA:480x800
+# drm_kms_helper.drm_fbdev_overalloc=300
+fatload mmc 0:1 0x46000000 zImage
+fatload mmc 0:1 0x49000000 ${fdtfile}
+bootz 0x46000000 - 0x49000000
