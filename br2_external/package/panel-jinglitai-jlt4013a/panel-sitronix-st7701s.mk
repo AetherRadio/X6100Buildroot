69c8062dd469ecf19810ad1af2b0fbdf418d7b35
Rui Oliveira <ruimail24@gmail.com>
2022-11-27 10:18:10 +0000

    Rename the Jinglitai package

---
+++ br2_external/package/panel-jinglitai-jlt4013a/panel-sitronix-st7701s.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+#  Driver for the Jinglitai JLT4013A LCD Panel.
+#
+################################################################################
+
+PANEL_JINGLITAI_JLT4013A_VERSION = master
+PANEL_JINGLITAI_JLT4013A_SOURCE = $(PANEL_JINGLITAI_JLT4013A_VERSION).tar.gz
+PANEL_JINGLITAI_JLT4013A_SITE = https://github.com/ruilvo/panel-jinglitai-jlt4013a/archive
+PANEL_JINGLITAI_JLT4013A_AUTORECONF = YES
+PANEL_JINGLITAI_JLT4013A_LICENSE = GPLv2
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
