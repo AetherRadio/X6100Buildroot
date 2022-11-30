198ea1e6d66075c7b1d35cdb870c89dac5cb2db8
Rui Oliveira <ruimail24@gmail.com>
2022-11-30 08:22:02 +0000

    Add agressive logging for the DRM drivers

--- br2_external/board/X6100/u-boot/boot.cmd
+++ br2_external/board/X6100/u-boot/boot.cmd
@@ -1,5 +1,5 @@
 echo "------------ X6100 boot script ------------"
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 fbcon=rotate:3
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10 fbcon=rotate:3 drm.debug=0x1f ignore_loglevel
 fatload mmc 0:1 0x46000000 zImage
 fatload mmc 0:1 0x49000000 ${fdtfile}
 bootz 0x46000000 - 0x49000000
