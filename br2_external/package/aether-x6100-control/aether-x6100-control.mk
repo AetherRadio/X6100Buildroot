b0cf8dd51e8f5fb341bd1c8f53cbf059856e62ed
Rui Oliveira <ruimail24@gmail.com>
2022-12-16 17:37:08 +0000

    Update version of X6100 control

--- br2_external/package/aether-x6100-control/aether-x6100-control.mk
+++ br2_external/package/aether-x6100-control/aether-x6100-control.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AETHER_X6100_CONTROL_VERSION = f09dd3119da3e2e1f3c1d0d80630659fc5696069
+AETHER_X6100_CONTROL_VERSION = b9cc18f25d80f68c672732192b73d3bcb51a981f
 AETHER_X6100_CONTROL_SOURCE = $(AETHER_X6100_CONTROL_VERSION).tar.gz
 AETHER_X6100_CONTROL_SITE = https://github.com/AetherRadio/X6100Control/archive
 AETHER_X6100_CONTROL_LICENSE = GPLv2
