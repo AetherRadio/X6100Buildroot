ac56610a04627e7e13d25095dcc551646b097a1d
Rui Oliveira <ruimail24@gmail.com>
2022-12-16 16:15:33 +0000

    Use a commit hash as the package version

--- br2_external/package/aether-x6100-control/aether-x6100-control.mk
+++ br2_external/package/aether-x6100-control/aether-x6100-control.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AETHER_X6100_CONTROL_VERSION = master
+AETHER_X6100_CONTROL_VERSION = f09dd3119da3e2e1f3c1d0d80630659fc5696069
 AETHER_X6100_CONTROL_SOURCE = $(AETHER_X6100_CONTROL_VERSION).tar.gz
 AETHER_X6100_CONTROL_SITE = https://github.com/AetherRadio/X6100Control/archive
 AETHER_X6100_CONTROL_LICENSE = GPLv2
